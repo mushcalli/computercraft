9ec95a3ee2bebb008ae698919a830c6a8b8559a7
Calli Kiler <mushcalli@gmail.com>
2024-05-08 02:40:58 -0700

    text tweak

--- play-music.lua
+++ play-music.lua
@@ -81,7 +81,7 @@ while true do
         end
     end
 
-    print("1-0: play song, W: add song, E: edit song, D: delete song")
+    print("\n\n1-0: play song, W: add song, E: edit song, D: delete song")
 
     local event, key = os.pullEvent("key_up")
     if (key >= keys.zero and key <= keys.nine and #music_list ~= 0) then
