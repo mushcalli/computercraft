73cd648b453da61371cd91924204c023a300bf31
Calli Kiler <mushcalli@gmail.com>
2024-08-12 04:33:34 -0700

    Update organSetup.lua

---
+++ organSetup.lua
@@ -0,0 +1,11 @@
+shell.run("wget https://github.com/noodle2521/computercraft/raw/main/organChunk.lua")
+
+local num = read()
+
+local file = fs.open("offset.txt", "w")
+file.writeLine(num)
+file.close()
+
+file = fs.open("startup.lua", "w")
+file.writeLine('shell.run("organChunk.lua")')
+fs.close()
