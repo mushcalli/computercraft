3dad360205e447cc0a0286f4e2b33bfa8dfce21d
Calli Kiler <mushcalli@gmail.com>
2025-01-05 21:55:24 -0800

    Create atm9-server.lua

---
+++ atm9-server.lua
@@ -0,0 +1,58 @@
+local chat = peripheral.find("chatBox")
+if (not chat) then
+    error("error: chatBox not found")
+end
+
+local function runInWindow(input)
+    local win = window.create(term.current(), 1, 1, term.getSize())
+    local old = term.redirect(win)
+    local ok = shell.run(input)
+    return win, ok
+end
+
+local function messageCalli(msg, isError)
+    if (not isError) then
+        chat.sendToastToPlayer(msg, "^w^", "bonjour_baguette", "&bcyber irys", "<>", "&3")
+    else
+        chat.sendToastToPlayer("&c" .. msg, "@_@", "bonjour_baguette", "&bcyber-irys", "<>", "&3")
+    end
+
+    if (string.find(msg, "\n")) then
+        chat.sendMessageToPlayer(msg, "bonjour_baguette", "&bcyber-irys", "<>", "&3")
+    end
+end
+
+--- main
+while true do
+    parallel.waitForAny()
+end
+
+local function runChatCommands()
+    local event, command = os.pullEvent("chatCommand")
+    local win, ok = runInWindow(command)
+    local out = ""
+    local _, lines = win.getSize()
+    for i = 1, lines do
+        out = out .. win.getLine(i)
+    end
+    
+    messageCalli(out, ok)
+end
+
+local function catchChatEvents()
+    local event, user, msg, _, hidden = os.pullEvent("chat")
+    local command = nil
+    if (hidden) then
+        command = msg
+    elseif (string.sub(msg, 1, 1) == ";") then
+        command = string.sub(msg, 2)
+    end
+    if (command) then
+        if (command == "kill") then
+            messageCalli("rebooting server,,,", true)
+            os.reboot()
+        end
+
+        os.queueEvent("chatCommand", command)
+    end
+end
