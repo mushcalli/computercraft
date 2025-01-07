92bd97bc443f0b7705493521bd88015bcffcfbda
Calli Kiler <mushcalli@gmail.com>
2025-01-07 02:51:05 -0800

    added face randomizer :)

--- calliMessenger.lua
+++ calliMessenger.lua
@@ -5,16 +5,40 @@ end
 
 local calliMessenger = {}
 
+local regFaces = {
+    "^w^",
+    "*u*",
+    "OwO",
+    "UwU",
+    "OwU",
+    "::::D",
+    ":33",
+    ">:3"
+}
+
+local errFaces = {
+    "@_@",
+    "XnX",
+    "0x0",
+    "~m~"
+}
+
+math.randomseed(os.time())
+
 function calliMessenger.messageCalli(msg, isError)
     if (string.find(msg, "\n")) then
-        chat.sendMessageToPlayer("\n" .. msg, "bonjour_baguette", "&bcyber-irys", "<>", "&3")
+        if (not isError) then
+            chat.sendMessageToPlayer(regFaces[math.random(1, #regFaces)] .. "\n" .. msg, "bonjour_baguette", "&bcyber-irys", "<>", "&3")
+        else
+            chat.sendMessageToPlayer(errFaces[math.random(1, #errFaces)] .. "\n§c" .. msg, "bonjour_baguette", "&bcyber-irys", "<>", "&3")
+        end
         return
     end
 
     if (not isError) then
-        chat.sendToastToPlayer(msg, "^w^", "bonjour_baguette", "&bcyber irys", "<>", "&3")
+        chat.sendToastToPlayer(msg, regFaces[math.random(1, #regFaces)], "bonjour_baguette", "&bcyber irys", "<>", "&3")
     else
-        chat.sendToastToPlayer("§c" .. msg, "@_@", "bonjour_baguette", "&bcyber-irys", "<>", "&3")
+        chat.sendToastToPlayer("§c" .. msg, errFaces[math.random(1, #errFaces)], "bonjour_baguette", "&bcyber-irys", "<>", "&3")
     end
 end
 
