cb3880f4f6ae228d920889b6dd87a0bf648ac2be
Calli Kiler <mushcalli@gmail.com>
2025-01-31 01:23:24 -0800

    Update mana.lua

--- atm9-server-programs/mana.lua
+++ atm9-server-programs/mana.lua
@@ -3,6 +3,6 @@ rednet.broadcast("t mana", "CawwiCorp-mana")
 local id, message = rednet.receive("CawwiCorp-mana")
 if (message == "t") then
     print("enabling mana generation/netherite farm")
-else
-    print("enabling mana generation/netherite farm")
+elseif (message == "f") then
+    print("disabling mana generation/netherite farm")
 end
